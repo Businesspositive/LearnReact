686bbcade3cef26db5c6a8a50a10149e774ff3d3
Vishwajeetthedev <vishwajeet@lolly.com>
2023-10-03 21:26:32 +0530

    installed react-router-dom and implemented routers to handle different urls in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,47 +1,55 @@
-import { useState } from 'react';
-import './App.css';
-// import About from './components/About';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
-import Alert from './components/Alert';
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light')
-  const [alert, setAlert]=useState(null)
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
-    setTimeout(()=>{
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
-  const toggleMode =()=>{
-    if (mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#7d4ed1';
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#7d4ed1";
       showAlert("Dark Mode has been enabled", "success");
-      document.title = "TextUtils-Darkmode"
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+      document.title = "TextUtils-Darkmode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light Mode has been enabled", "success");
-      document.title = "TextUtils-Lightmode"
-
+      document.title = "TextUtils-Lightmode";
     }
-  }
+  };
   return (
     <>
-    {/* Passing the component with props in main app */}
-    <Navbar title = "TextUtils" aboutText = "About" mode = {mode} toggleMode = {toggleMode}/>
-    <Alert alert = {alert}/>
-    <div className="container">
-      <Textform heading = "Enter the text to convert" mode={mode} showAlert = {showAlert}/>
-      {/* <About/> */}
-    </div>
+      {/* Passing the component with props in main app */}
+      <Router>
+      <Navbar
+        title="TextUtils"
+        aboutText="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container">
+        <Routes>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/" element={<Textform heading="Enter the text to convert" mode={mode} showAlert={showAlert}/>}/>
+        </Routes>
+      </div>
+      </Router>
     </>
   );
 }
