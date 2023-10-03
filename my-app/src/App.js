c6d8f745ceb33b467138f414039f14e8d7e1daef
Vishwajeetthedev <vishwajeet@lolly.com>
2023-10-03 14:46:13 +0530

    added dark and light mode and made the title of the website dynamic inside the mode chnaged the icons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,45 @@
+import { useState } from 'react';
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
+import Alert from './components/Alert';
+
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert]=useState(null)
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode =()=>{
+    if (mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#7d4ed1';
+      showAlert("Dark Mode has been enabled", "success");
+      document.title = "TextUtils-Darkmode"
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+      document.title = "TextUtils-Lightmode"
+
+    }
+  }
   return (
     <>
     {/* Passing the component with props in main app */}
-    <Navbar title = "My React App" aboutText = "About"/>
+    <Navbar title = "TextUtils" aboutText = "About" mode = {mode} toggleMode = {toggleMode}/>
+    <Alert alert = {alert}/>
     <div className="container">
-      <Textform heading = "Enter the text to convert"/>
+      <Textform heading = "Enter the text to convert" mode={mode} showAlert = {showAlert}/>
       {/* <About/> */}
     </div>
     </>
--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -8,16 +8,21 @@ export default function Textform(props) {
     // console.log("UpperCase was clicked");
     let newText = text.toUpperCase();
     setText(newText)
+    props.showAlert("Converted to Uppercase", "success")
   }
 
   const handleLoClick = ()=>{
     let newText = text.toLowerCase();
     setText(newText)
+    props.showAlert("Converted to Lowercase", "success")
+
   }
 
   const handleClearClick = ()=>{
     let newText = text.substring(0,0);
     setText(newText)
+    props.showAlert("Cleared the Text", "success")
+
   }
 
   const handleOnChange = (event)=>{
@@ -32,15 +37,17 @@ export default function Textform(props) {
     <>
     <div>
         <h1>{props.heading}</h1>
-        <div className="mb-3">
-            <textarea id="myBox" value = {text} onChange={handleOnChange} rows="10" className="form-control"></textarea>
+        <div className="mb-3" style = {{backgroundColor:props.mode==='dark'?'white':'black'}}>
+            <textarea id="myBox" value = {text} onChange={handleOnChange} rows="10" className="form-control" style = {{backgroundColor:props.mode==='dark'?'grey':'white',
+          color:props.mode==='dark'?'white':'black'}}></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lowercase</button>
         <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
 
     </div>
-    <div className="container my-2">
+    <div className="container my-2" style = {{backgroundColor:props.mode==='dark'?'grey':'white',
+          color:props.mode==='dark'?'white':'black'}}>
       <h2>Your text summary</h2>
       <p>{text.split(" ").length} Word and {text.length} Charectors</p>
       <p>{(text.split(" ").length)*0.008} minutes to read</p>
